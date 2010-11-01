144e1aaf07a1e49e68c1f82ced724f2b0518b659
Vedat Hallac <vedathallac@gmail.com>
2010-11-01 21:44:35 +0200

    ADD: blueprint stylesheets

--- sample_app/lib/helpers.py
+++ sample_app/lib/helpers.py
@@ -4,4 +4,13 @@ Consists of functions to typically be used within templates, but also
 available to Controllers. This module is available to templates as 'h'.
 """
 # Import helpers as desired, or define your own, ie:
-#from webhelpers.html.tags import checkbox, password
+from webhelpers.html.tags import stylesheet_link
+
+from pylons import tmpl_context as c
+
+def title():
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if c.title is None:
+        return base_title
+    else:
+        return "%s | %s"%(base_title, c.title)
--- sample_app/templates/base/application.mako
+++ sample_app/templates/base/application.mako
@@ -2,7 +2,9 @@
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">
   <head>
-    <title>Ruby on Rails Tutorial Sample App | ${c.title}</title>
+    <title>${h.title()}</title>
+    ${h.stylesheet_link('/stylesheets/blueprint/screen.css', media='screen') }
+    ${h.stylesheet_link('/stylesheets/blueprint/print.css', media='print') }
   </head>
   <body>
     ${self.body()}
